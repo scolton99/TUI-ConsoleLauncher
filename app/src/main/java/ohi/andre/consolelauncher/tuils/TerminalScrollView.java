729bfbcdb34df37a89390b5f61e498c4fbdbbfc0
Spencer Colton <me@scolton.tech>
2025-01-19 23:36:06 -0600

    Clean things up -- still some judder when flinging

--- app/src/main/java/ohi/andre/consolelauncher/tuils/TerminalScrollView.java
+++ app/src/main/java/ohi/andre/consolelauncher/tuils/TerminalScrollView.java
@@ -2,23 +2,19 @@ package ohi.andre.consolelauncher.tuils;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 public class TerminalScrollView extends ScrollView {
     private int lineHeight = 1;
 
     private boolean isSnapping = false;
-
-    public TerminalScrollView(Context context) {
-        super(context);
-    }
+    private int lastRem = 0;
 
     public TerminalScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
-    }
-
-    public TerminalScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
     }
 
     public TerminalScrollView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
@@ -37,11 +33,38 @@ public class TerminalScrollView extends ScrollView {
     }
 
     @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        int paddingTop = h % this.lineHeight;
+        this.getChildAt(0).setPadding(0, paddingTop, 0, 0);
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    /**
+     *
+     * @param l Current horizontal scroll origin.
+     * @param t Current vertical scroll origin.
+     * @param oldl Previous horizontal scroll origin.
+     * @param oldt Previous vertical scroll origin.
+     */
+    @Override
     protected void onScrollChanged(int l, int t, int oldl, int oldt) {
         super.onScrollChanged(l, t, oldl, oldt);
         if (isSnapping)
             return;
 
-        this.scrollTo(l,(t / this.lineHeight) * this.lineHeight);
+        // Round to a multiple of lineHeight
+        // TODO: should this be a floor? A ceil? A round?
+        int newHeight = (t / this.lineHeight) * this.lineHeight;
+
+        // The user tried to scroll further in one direction -- probably. How much did we cut them off?
+        // Have to remember this so that if the scrolling is slow -- think 1px per call of this function,
+        // we still get a snap to the next line eventually...
+        int rem = t - newHeight;
+        int cumulativeRem = rem + lastRem;
+
+        newHeight += (cumulativeRem / this.lineHeight) * this.lineHeight;
+        this.lastRem = cumulativeRem % this.lineHeight;
+
+        this.scrollTo(l, newHeight);
     }
 }
